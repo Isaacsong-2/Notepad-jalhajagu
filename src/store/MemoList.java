a3002cdbabf405735a6e02e8372d28be3a5758b3
SongIsak <thddltkr10@naver.com>
2023-06-05 09:33:57 +0900

    init

--- src/store/MemoList.java
+++ src/store/MemoList.java
@@ -1,4 +1,36 @@
 package store;
 
+import entity.Memo;
+
+import java.util.ArrayList;
+
 public class MemoList {
+    private ArrayList<Memo> memos;
+
+    public MemoList() {
+        this.memos = new ArrayList<>();
+    }
+
+//    @Override
+    public void create(Memo memo) {
+    }
+
+//    @Override
+    public Memo retrieve(int postNum) {
+        return null;
+    }
+
+//    @Override
+    public void update(Memo updateMemo) {
+    }
+
+//    @Override
+    public void delete(int postNum) {
+
+    }
+
+//    @Override
+    public ArrayList<Memo> getMemos() {
+        return null;
+    }
 }
