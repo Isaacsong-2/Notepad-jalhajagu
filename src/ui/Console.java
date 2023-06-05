64eafed875109046630511b2a984c8b006f4d411
chohaena <johanna0123@naver.com>
2023-06-05 13:22:04 +0900

    메모 조회 기능
    메모 전체 최신순 조회 및 출력하기

--- src/ui/Console.java
+++ src/ui/Console.java
@@ -4,6 +4,8 @@ import entity.Memo;
 import store.MemoList;
 import util.ConsoleUtil;
 
+import java.util.ArrayList;
+
 public class Console {
     private ConsoleUtil consoleUtil;
 
@@ -17,7 +19,11 @@ public class Console {
     public void register() {
     }
 
-    public void read() {
+    public void read() { // 메모 조회 기능
+        ArrayList<Memo> memos = memoList.getMemos();
+        for (int i = memos.size() - 1; i > -1; i--) {
+            System.out.println(memos.get(i));
+        }
     }
 
     private Memo findOne() {
