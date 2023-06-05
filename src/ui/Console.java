cd099571643d8feb9b54ee4a98e4103b76c7b01f
hee0hee <132984807+hee0hee@users.noreply.github.com>
2023-06-05 17:38:40 +0900

    delete 구현
    인터넷과 자료들을 찾아가 보면서 delete 구현을 해봤습니다
    맞는지 모르겠습니다ㅠㅠ

--- src/ui/Console.java
+++ src/ui/Console.java
@@ -21,7 +21,24 @@ public class Console {
     }
 
     private Memo findOne() {
-        return null;
+        Memo memo = null;
+        // 초기값을 주지 않으면 쓰레기값이 들어감, 필수
+        int postNum = Integer.parseInt(consoleUtil.getValueOf("수정하실 글의 번호를 입력해주세요"));
+        // Integer.parseInt 문자열을 정수형으로 변환하는 역할, 게시글 번호를 나타내는 값
+        // consoleUtil.getValueOf의 값(수정할 글 번호)를 불러옴
+
+        memo = memoList.retrieve(postNum);
+        //memo는 memoList에 있는 retrieve(int postNum)이다
+
+        if (memo == null) {
+            System.out.println("해당 게시글이 존재하지 않습니다.");
+        } else {
+            System.out.println(memo.toString());
+            //else는 게시글이 존재할 때 memo.toString() = memo 객체의 내용을 가지고 있는 문자열을 반환
+            //-> 게시글 번호 나옴?
+
+        }
+        return memo;
     }
 
     public void update() {
@@ -29,6 +46,29 @@ public class Console {
     }
 
     public void delete() {
+        Memo memo = findOne();
+        //findOne을 불러서 delete 할 게시글을 탐색
+        if (memo == null) {
+            System.out.println("해당 게시글이 존재하지 않습니다.");
+            return;
+        }//만약 memo가 null이라면 sout 출력
+
+        System.out.println("게시글 삭제");
+        String password = consoleUtil.getValueOf("비밀번호 입력");
+        //sout이 출력되고 비밀번호 입력하기
+        //consoleUtil에 있는 getValueOf는 불러와서 password 입력할 수 있게 해줌?
+
+        if (!password.equals(memo.getPassword())) {
+            System.out.println("비밀번호가 틀립니다.");
+            return;
+        }
+
+        memoList.delete(memo.getPostNum());
+        System.out.println("게시글이 성공적으로 삭제되었습니다.");
+        // getPostNum(게시물 넘버) 삭제 완료??!
+    }
+
+
+
 
     }
-}
