392c07750f2cf726ed64bddcfd8133755d0d428c
Eunbi Kim <eunbkim4@gmail.com>
2023-06-05 19:13:59 +0900

    fix: 콘솔 출력 버그 수정

--- src/ui/Console.java
+++ src/ui/Console.java
@@ -35,7 +35,7 @@ public class Console {
     }
 
     private Memo findOne() {
-        int postNum = Integer.parseInt(consoleUtil.getValueOf("수정할 글 번호를 입력해주세요: "));
+        int postNum = Integer.parseInt(consoleUtil.getValueOf("수정할 글 번호를 입력해주세요"));
         return memoList.retrieve(postNum)
                 .orElseThrow(() -> new MemoNotFoundException(postNum + "번에 해당하는 메모가 존재하지 않습니다."));
     }
@@ -61,16 +61,16 @@ public class Console {
 //    }
     public void update() {
         Memo memo = findOne();
-        String password = consoleUtil.getValueOf("비밀번호를 입력해주세요: ");
+        String password = consoleUtil.getValueOf("비밀번호를 입력해주세요");
         if(memo.getPassword().equals(password)) {
-            System.out.println("기존 메모 : \n" + memo.getPost());
-            String updatedPost = consoleUtil.getValueOf("메모를 수정해주세요: \n");
+            System.out.println("기존 메모 : " + memo.getPost());
+            String updatedPost = consoleUtil.getValueOf("수정된 메모");
             memo.setPost(updatedPost);
             System.out.println("수정 완료되었습니다.");
         } else {
             System.out.println("비밀번호가 일치하지 않습니다.");
         }
-    }
+    }1
 
     public void delete() {
         Memo memo = findOne();
